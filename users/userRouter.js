c2f2f7f3f99bfded6945276c8b9c6ac68cd9ca60
admin <admin@admins-MacBook-Pro.local>
2019-10-30 13:05:38 -0700

    userRouter route handlers completed

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,87 @@
 const express = require('express');
-
+const db = require('./userDb.js')
 const router = express.Router();
 
 router.post('/', (req, res) => {
-
+     
+     
 });
+
+
 
 router.post('/:id/posts', (req, res) => {
 
 });
 
+
+
+
+// * GET all users
 router.get('/', (req, res) => {
-
+     const query = req.query
+     db.get(query)
+     .then(users => {
+          res.status(200).json(users)
+     })
+     .catch(error => {
+          res.status(500).json({message: "Users could not be retrieved"})
+     })
 });
 
+// * GET user by ID
 router.get('/:id', (req, res) => {
-
+     const id = req.params.id
+    
+     db.getById(id)
+     .then(users => {
+          res.status(200).json(users)
+     })
+     .catch(error => {
+          res.status(500).json({message: "User could not be retrieved"})
+     })
 });
 
+
+// * GET posts by userID
 router.get('/:id/posts', (req, res) => {
+     const id = req.params.id 
 
+     db.getUserPosts(id)
+     .then(posts => {
+          res.status(200).json(posts)
+     })
+     .catch(error => {
+          res.status(500).json({message: "Posts from user could not be retrieved"})
+     })
 });
 
+
+// * DELETE user by ID
 router.delete('/:id', (req, res) => {
+     const id = req.params.id 
 
+     db.remove(id)
+     .then(user => {
+          res.status(200).json(user)
+     })
+     .catch(error => {
+          res.status(500).json({message: "User was unsuccessfully deleted"})
+     })
 });
 
-router.put('/:id', (req, res) => {
 
+// * UPDATE user by ID
+router.put('/:id', (req, res) => {
+     const id = req.params.id
+     const updateInfo = req.body
+
+     db.update(id, updateInfo)
+     .then(user => {
+          res.status(200).json(user)
+     })
+     .catch(error => {
+          res.status(500).json({message: "User was unsuccessfully updated"})
+     })
 });
 
 //custom middleware
